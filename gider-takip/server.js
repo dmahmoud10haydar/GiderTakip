a25e72215e49e9f7e00e98221c4dc470f904abbb
Mahmoud Haydar <d.mahmoud10haydar@gmail.com>
2025-06-02 10:12:24 +0300

    GiderTakip projemin dosyalari eklendi

---
+++ gider-takip/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Gider Takip Uygulaması Başladı!');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Sunucu çalışıyor: http://localhost:${PORT}`);
+});
+
+const mongoose = require('mongoose');
+mongoose
+  .connect('mongodb+srv://dmahmoud10haydar:pCmIUhpmgMqEQL14@cluster0.0x96cqp.mongodb.net/giderTakipDB')
+  .then(() => console.log('MongoDB Atlas bağlantısı başarılı'))
+  .catch((err) => console.error('MongoDB bağlantı hatası:', err));
